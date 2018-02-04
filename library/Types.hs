e2461db7d7d51bb762e43efccd838640f1d8268f
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2018-02-03 18:18:50 -0800

    Work on types.

--- library/Types.hs
+++ library/Types.hs
@@ -7,11 +7,14 @@ import Data.IORef
 import qualified Data.Text as Text
 import qualified Control.Concurrent.STM as STM
 import qualified Data.Map.Strict as Map
-
+--import qualified Control.Monad.IO.Class.MonadIO as MonadIO
 
 data AppSession = EmptySession
 
 data AppState = AppState (STM.TVar (Map.Map Text.Text Text.Text))
 
 type AppM a = SpockM () AppSession AppState a
-type ActionCtx ctx a = SpockActionCtx ctx () AppSession AppState a
+
+-- type ActionCtx ctx a = SpockActionCtx ctx () AppSession AppState a
+--type AppActionCtx = MonadIO m => ActionCtxT ctx m a
+--type AppActionCtx = ActionCtxT ctx m a
--- library/Views.hs
+++ library/Views.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
-module Views where
-
-import Types
-import Web.Spock
-
+module Views(getCollection) where
 import qualified Data.Text as Text
+import Web.Spock
+import Types
+import Control.Monad.IO.Class (MonadIO)
 import Data.Monoid
 
 -- ActionCtxT ctx (WebStateM conn sess st) () -> SpockCtxM ctx conn sess st ()
-getCollection :: ActionCtxT ctx AppM ()
-getCollection = do
+-- getCollection :: ActionCtxT ctx AppM ()
+
+-- getCollection :: ActionCtxT ctx m0 a
+getCollection :: MonadIO m => Text.Text -> ActionCtxT ctx m a
+getCollection a = do
   (AppState ref) <- getState
-  text ("Hello " <> name <> ", you are visitor number " <> Text.pack (show 0))
+  text ("Hello " <> a <> ", you are visitor number 0 ")
