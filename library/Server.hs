11311d932a3e8298437d8938cbf8061c3013f78c
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2018-02-27 22:13:18 -0800

    Update monitoring to include wai metrics.

--- library/Server.hs
+++ library/Server.hs
@@ -11,25 +11,35 @@ import Types
 import qualified Views as Views
 import qualified Env as Env
 import System.Remote.Monitoring
-import Control.Monad(when)
+import Network.Wai.Metrics
+
+-- import Control.Monad(when)
 import Data.Monoid((<>))
 import qualified Data.ByteString.Char8 as C8
 
 main :: IO ()
 main = do
   config <- Env.readConfig
-  when (monitoringEnabled config) $ do
-    let monIp=(monitoringIP config)
-        monPort=(monitoringPort config)
-    _ <- forkServer monIp monPort
-    putStrLn ("Monitoring is enabled at " <> (C8.unpack monIp) <> ":" <>(show monPort))
-  runSpock (port config) (getApp config)
+  r <- if (monitoringEnabled config) then do
+         let monIp=(monitoringIP config)
+             monPort=(monitoringPort config)
+         ekg <- forkServer monIp monPort
+         waiMetrics <- registerWaiMetrics (serverMetricStore ekg)
+         putStrLn ("Monitoring is enabled at " <> (C8.unpack monIp) <> ":" <>(show monPort))
+         let r = do
+               (middleware (metrics waiMetrics))
+               routes
+         return r
+       else
+         return routes
+  runSpock (port config) (getApp config r)
 
-getApp :: Config -> IO Middleware
-getApp config =
+--getApp :: Config -> WaiMetrics -> IO Middleware
+getApp :: Config -> SpockM () AppSession AppState () -> IO Middleware
+getApp config r =
     do ref <- STM.atomically $ STM.newTVar Map.empty
        spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (AppState ref config)
-       spock spockCfg routes
+       spock (spockCfg{ spc_maxRequestSize=Nothing }) r
 
 routes :: SpockM () AppSession AppState ()
 routes =
